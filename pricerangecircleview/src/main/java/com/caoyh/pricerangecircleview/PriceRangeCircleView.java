739b37d7469cd24d357e1bcb80cc3a25ee075231
vito-cao <tobedn@163.com>
2018-01-25 20:35:31 +0800

    add watermarkview

---
+++ pricerangecircleview/src/main/java/com/caoyh/pricerangecircleview/PriceRangeCircleView.java
@@ -0,0 +1,189 @@
+package com.caoyh.pricerangecircleview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by caoyh
+ * Created on 2016/4/20
+ * Description:
+ */
+public class PriceRangeCircleView extends View {
+
+    private Paint mPaint;
+    private Paint mTextPaint;
+    private static final int DEFAULT_RADIUS = 75; //75px,setStrokeWidth=15
+    private static final int DEFAULT_PERCENT_SIZE = 44; //44px
+    private static final int DEFAULT_BOTTOM_SIZE = 28; //28px
+
+    private int mDefaultColor;
+    private int mProgressColor;
+    private int mPercentColor;
+    private int mBottomColor;
+    private float mBottomTextSize;
+    private float mPercentTextSize;
+    private String mPercent;
+    private String mBottomText;
+    private int mRadius;
+    private int mCenterX;
+    private int mCenterY;
+    private RectF percentRectF;
+    private boolean isShortBottom;
+    private int bottomTextWith;
+
+    public PriceRangeCircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public PriceRangeCircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(30);
+
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.PriceRangeCircleView);
+//
+        mRadius = a.getDimensionPixelSize(R.styleable.PriceRangeCircleView_circle_radius,
+                DEFAULT_RADIUS);
+        mPercentTextSize = a.getDimensionPixelSize(R.styleable.PriceRangeCircleView_percent_text_size,
+                DEFAULT_PERCENT_SIZE);
+        mBottomTextSize = a.getDimensionPixelSize(R.styleable.PriceRangeCircleView_bottom_text_size,
+                DEFAULT_BOTTOM_SIZE);
+
+        mDefaultColor = a.getColor(R.styleable.PriceRangeCircleView_default_color, Color.parseColor("#e5e5e5"));
+        mProgressColor = a.getColor(R.styleable.PriceRangeCircleView_progress_color, Color.parseColor("#ff6600"));
+        mPercentColor = a.getColor(R.styleable.PriceRangeCircleView_percent_text_color, Color.parseColor("#333333"));
+        mBottomColor = a.getColor(R.styleable.PriceRangeCircleView_bottom_text_color, Color.parseColor("#999999"));
+        mTextPaint.setTextSize(mBottomTextSize);
+
+        a.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec));
+    }
+
+    private int measureWidth(int measureSpec) {
+        int result;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            bottomTextWith = (int) mTextPaint.measureText(mBottomText);
+            if (bottomTextWith > (mRadius * 2 + mPaint.getStrokeWidth())) {
+                isShortBottom = false;
+                result = bottomTextWith + getPaddingLeft() + getPaddingRight();
+            } else {
+                result = (int) (mRadius * 2 + mPaint.getStrokeWidth() +
+                        getPaddingLeft() + getPaddingRight());
+                isShortBottom = true;
+            }
+
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+
+        return result;
+    }
+
+    private int measureHeight(int measureSpec) {
+        int result;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = (int) (mRadius * 2 + mPaint.getStrokeWidth() + 20 - mTextPaint.ascent()
+                    + mTextPaint.descent()) + getPaddingTop() + getPaddingBottom();
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+
+        }
+        return result;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (isShortBottom) {
+            mCenterX = getPaddingLeft() + (int) (mPaint.getStrokeWidth() / 2) + mRadius;
+        } else {
+            mCenterX = getPaddingLeft() + bottomTextWith/2;
+        }
+
+        mCenterY = getPaddingTop() + (int) (mPaint.getStrokeWidth() / 2) + mRadius;
+        percentRectF = new RectF(mCenterX - mRadius, mCenterY - mRadius,
+                mCenterX + mRadius, mCenterY + mRadius);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //center radius,30dp,45dp
+        //圆
+        mPaint.setColor(mDefaultColor);
+        canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaint);
+        if (!TextUtils.isEmpty(mPercent)) {
+            //进度
+            mPaint.setColor(mProgressColor);
+            canvas.drawArc(percentRectF, -90, Float.valueOf(mPercent) / 100 * 360, false, mPaint);
+
+            //圆心字体
+            mTextPaint.setColor(mPercentColor);
+            mTextPaint.setTextSize(mPercentTextSize);
+
+            //文字中心绘制在圆心
+            float percentTextWidth = mTextPaint.measureText(mPercent + "%");
+            float percentTextX = mCenterX - percentTextWidth / 2;
+            float percentTextY = mCenterY - (-mTextPaint.ascent() + mTextPaint.descent()) / 2 - mTextPaint.ascent();
+            canvas.drawText(mPercent + "%", percentTextX, percentTextY, mTextPaint);
+        }
+
+        //底部字体
+        mTextPaint.setTextSize(mBottomTextSize);
+        mTextPaint.setColor(mBottomColor);
+        if (!TextUtils.isEmpty(mBottomText)) {
+            float bottomTextWidth = mTextPaint.measureText(mBottomText);
+            //////
+            float bottomBaseLine = mCenterY + mRadius + mPaint.getStrokeWidth() / 2 + 20 - mTextPaint.ascent();
+
+
+            canvas.drawText(mBottomText, mCenterX - bottomTextWidth / 2, bottomBaseLine, mTextPaint);
+        }
+    }
+
+    public PriceRangeCircleView setPercent(String percent) {
+        mPercent = percent;
+        return this;
+    }
+
+    public PriceRangeCircleView setBottomText(String bottomText) {
+        mBottomText = bottomText;
+        return this;
+    }
+
+    public void draw() {
+        requestLayout();
+        invalidate();
+    }
+}
---
+++ watermark/src/main/java/com/caoyh/watermark/WatermarkView.java
@@ -0,0 +1,166 @@
+package com.caoyh.watermark;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class WatermarkView extends View {
+    private String mExampleString; // TODO: use a default from R.string...
+    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
+    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
+    private Drawable mExampleDrawable;
+
+    private TextPaint mTextPaint;
+    private float mTextWidth;
+    private float mTextHeight;
+
+    public WatermarkView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public WatermarkView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public WatermarkView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.WatermarkView, defStyle, 0);
+
+        mExampleString = a.getString(
+                R.styleable.WatermarkView_textContent);
+        mExampleColor = a.getColor(
+                R.styleable.WatermarkView_textColor,
+                mExampleColor);
+        mExampleDimension = a.getDimension(
+                R.styleable.WatermarkView_textSize,
+                mExampleDimension);
+
+        a.recycle();
+
+        // Set up a default TextPaint object
+        mTextPaint = new TextPaint();
+        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTextAlign(Paint.Align.LEFT);
+
+        // Update TextPaint and text measurements from attributes
+        invalidateTextPaintAndMeasurements();
+    }
+
+    private void invalidateTextPaintAndMeasurements() {
+        mTextPaint.setTextSize(mExampleDimension);
+        mTextPaint.setColor(mExampleColor);
+        mTextWidth = mTextPaint.measureText(mExampleString);
+
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        mTextHeight = fontMetrics.bottom;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.translate(-800,0);
+        canvas.rotate(-30);
+
+        // Draw the text.
+        float startXPos = 0;
+        float startYPos = 0;
+        float textWCenter = mTextWidth / 2;
+        float textHCenter = mTextHeight / 2;
+        float lineWSpace = 100;
+        float lineHSpace = 100;
+        int lineTextNum = 5;
+
+        for (int i = 0; i < 200; i++) {
+
+            if (i % lineTextNum == 0) {
+                startXPos = 0;
+                startYPos += mTextHeight + lineHSpace;
+            }
+
+            canvas.drawText(mExampleString,
+                    startXPos,
+                    startYPos,
+                    mTextPaint);
+
+            startXPos += mTextWidth + lineWSpace;
+
+        }
+    }
+
+    /**
+     * Gets the example string attribute value.
+     *
+     * @return The example string attribute value.
+     */
+    public String getExampleString() {
+        return mExampleString;
+    }
+
+    /**
+     * Sets the view's example string attribute value. In the example view, this string
+     * is the text to draw.
+     *
+     * @param exampleString The example string attribute value to use.
+     */
+    public void setExampleString(String exampleString) {
+        mExampleString = exampleString;
+        invalidateTextPaintAndMeasurements();
+    }
+
+    /**
+     * Gets the example color attribute value.
+     *
+     * @return The example color attribute value.
+     */
+    public int getExampleColor() {
+        return mExampleColor;
+    }
+
+    /**
+     * Sets the view's example color attribute value. In the example view, this color
+     * is the font color.
+     *
+     * @param exampleColor The example color attribute value to use.
+     */
+    public void setExampleColor(int exampleColor) {
+        mExampleColor = exampleColor;
+        invalidateTextPaintAndMeasurements();
+    }
+
+    /**
+     * Gets the example dimension attribute value.
+     *
+     * @return The example dimension attribute value.
+     */
+    public float getExampleDimension() {
+        return mExampleDimension;
+    }
+
+    /**
+     * Sets the view's example dimension attribute value. In the example view, this dimension
+     * is the font size.
+     *
+     * @param exampleDimension The example dimension attribute value to use.
+     */
+    public void setExampleDimension(float exampleDimension) {
+        mExampleDimension = exampleDimension;
+        invalidateTextPaintAndMeasurements();
+    }
+}
